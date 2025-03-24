163889de359ebdbe1007ccce3474723b894fe831
YesseniaC <98070263+xOsitax@users.noreply.github.com>
2025-03-25 00:31:51 +0100

    updated
    dixed the disccount and change int for float

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 /*Ask user to input a price*/
-let price = prompt("Enfer the price :");
+let price = prompt("Enter the price :");
 /*Remove the dollar sign (converts to string)*/
-let priceN = parseInt(price.slice(1));
+let priceN = parseFloat(price.slice(1));
 /*Calculate the discount 10% */
 let discount = priceN * 0.9;
 /*Display the discounted price */
-console.log("The discounted price is : $" + discount);
+console.log(`New price is: $${discount.toFixed(2)}`);
